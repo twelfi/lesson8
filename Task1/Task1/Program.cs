17819b6ae818d4a1e7b4c484bea44888f9a2f0ff
Mysh <tipomysh.ru@mail.ru>
2025-01-07 13:19:15 +0300

    Done homework

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace Task1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите строку:");
+        string value = Console.ReadLine();
+        while (string.IsNullOrWhiteSpace(value))
+        {
+            Console.WriteLine("Некорректное значение");
+            value = Console.ReadLine();
+        }
+
+        StringBuilder sb = new StringBuilder();
+        foreach (char a in value)
+        {
+            if (!char.IsWhiteSpace(a))
+            {
+                sb.Append(a);
+            }
+        }
+
+        string result = sb.ToString().ToLower();
+        string reversed = new string(result.Reverse().ToArray());
+
+        if (result == reversed)
+        {
+            Console.WriteLine("Это палиндром");
+        }
+        else
+        {
+            Console.WriteLine("Это не палиндром");
+        }
+    }
+}
